e636fe9d380df3c97daa8492820374d952920c05
drmyersii <drmyersii@users.noreply.github.com>
2015-01-16 12:20:16 -0500

    Still working on templating.

---
+++ app/js/init.js
@@ -0,0 +1,35 @@
+;(function (window, document, undefined)
+{ 'use strict';
+
+	var helloText1 = 'Hello, World 1!', 
+		helloText2 = 'Hello, World 2!';
+
+	var hello1 = function ()
+	{
+		var html = window.JST['app/templates/hello.us']({text: helloText1});
+		
+		document.body.innerHTML += html;
+	}
+
+	var hello2 = function ()
+	{
+		var html = window.JST['app/templates/hello2.us']({text: helloText2});
+
+		document.body.innerHTML += html;
+	}
+
+	var init = function ()
+	{
+		hello1();
+		hello2();
+	}
+
+	if (window.addEventListener)
+	{
+		window.addEventListener('DOMContentLoaded', init, false);
+	}
+	else
+	{
+		window.attachEvent('onload', init);
+	}
+})(window, document);
